ded2931510e678e49032537f6034a6b5e87f9ea9
Ayush8923 <ayush.uniyal@coloredcow.in>
2024-07-19 12:49:31 +0530

    feat(*): setup moodle theme

---
+++ version.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version configuration for the theme academi
+ * @package    theme_academi
+ * @copyright  2024 onwards ColoredCow Team (https://coloredcow.com/)
+ * @author    ColoredCow Team
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+$plugin->version    = 2023100900; // This component release level.
+$plugin->maturity   = MATURITY_STABLE; // This version's maturity level.
+$plugin->release    = 'v4.4.1'; // This version's release version.
+$plugin->requires   = 2023100400; // This version's moodle require release.
+$plugin->component  = 'theme_academi'; // This component type_name.
+$plugin->dependencies = [];
