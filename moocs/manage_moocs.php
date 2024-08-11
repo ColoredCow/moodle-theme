fbe99d49c6d591bd99c1f338af45d0957585d05a
Ayush8923 <ayush.uniyal@coloredcow.in>
2024-08-11 22:30:54 +0530

    fix(*): moocs module

---
+++ moocs/manage_moocs.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once('../../../config.php');
+require_login();
+
+initialize_page();
+echo $OUTPUT->header();
+var_dump(get_string('pluginname', 'moocs'));
+
+echo $OUTPUT->footer();
+
+/**
+ * Initializes the page context and resources.
+ */
+function initialize_page() {
+    global $PAGE;
+
+    $context = context_system::instance();
+    $PAGE->set_context($context);
+    $PAGE->set_url(new moodle_url('/theme/academi/moocs/manage_moocs.php'));
+    $PAGE->set_title('MOOCs');
+    $PAGE->set_heading("MooCs");
+}
