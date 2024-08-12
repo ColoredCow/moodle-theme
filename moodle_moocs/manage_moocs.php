a22bac776f8d5e519538f508892e00840a404572
Ayush8923 <ayush.uniyal@coloredcow.in>
2024-08-12 22:51:24 +0530

    feat(*): init moocs page

--- moodle_moocs/manage_moocs.php
+++ moodle_moocs/manage_moocs.php
@@ -5,6 +5,7 @@ require_login();
 
 initialize_page();
 echo $OUTPUT->header();
+echo display_page(true);
 echo $OUTPUT->footer();
 
 /**
@@ -17,5 +18,17 @@ function initialize_page() {
     $PAGE->set_context($context);
     $PAGE->set_url(new moodle_url('/theme/academi/moodle_moocs/manage_moocs.php'));
     $PAGE->set_title('MOOCs');
-    $PAGE->set_heading("MooCs");
+}
+
+function display_page($moocs) {
+    global $OUTPUT;
+
+    // Include the HTML for the survey management interface
+
+    include(__DIR__ . '/templates/manage_moocs_header.php');
+    if ($moocs) {
+        include(__DIR__ . '/templates/manage_moocs_table.php');
+    } else {
+        echo html_writer::tag('div', 'No MOOCs Found.', ['class' => 'alert alert-info']);
+    }
 }
---
+++ moodle_moocs/templates/manage_moocs_header.php
@@ -0,0 +1,65 @@
+<?php
+    $categories = [];
+    $moocsstatusoptions = [];
+    $createurl = '#';
+    $createsurveycategoryurl = '#';
+    $createmoocscategoryurl = '#';
+    $iconurl = new \moodle_url('/local/moodle_survey/pix/plus-icon.svg');
+    $createbutton = html_writer::div(
+        html_writer::link(
+            $createmoocscategoryurl,
+            'MOOC categories',
+            array('class' => 'create-survey-button')
+        ) .
+        html_writer::link(
+            $createurl,
+            html_writer::tag('img', '', array('src' => $iconurl, 'alt' => 'Icon', 'class' => 'plus-icon')) . ' ' . 'Create new MOOC',
+            array('class' => 'create-survey-button')
+        ),
+        'create-survey-button-container'
+    );
+    $heading = html_writer::tag('span', 'MOOCs', ['class' => 'survey-name']);
+    $content = $heading . ' ' . $createbutton;
+    echo html_writer::tag('div', $content, ['class' => 'survey-header']);
+
+    // Filter form
+    $categoryoptions['all'] = 'Select Category';
+    $moocsstatusoptions['all']  = 'Select Status';
+
+    echo html_writer::start_tag('form', ['method' => 'get', 'action' => $PAGE->url, 'id' => 'filter-form']);
+    echo html_writer::start_div('filter-form d-flex justify-content-between');
+    echo html_writer::select($moocsstatusoptions, 'status', $status, null, ['class' => 'status-select', 'id' => 'status-select']);
+    echo html_writer::empty_tag('input', ['type' => 'date', 'name' => 'createdon', 'placeholder' => get_string('createdat', 'local_moodle_survey'), 'class' => 'date-input']);
+    echo html_writer::select($categoryoptions, 'category', $moocscategory, null, ['class' => 'status-select', 'id' => 'category-select']);
+
+    echo html_writer::empty_tag('input', ['type' => 'text', 'name' => 'search', 'value' => $search, 'placeholder' => get_string('search', 'local_moodle_survey'), 'class' => 'search-input']);
+
+    echo html_writer::end_div();
+    echo html_writer::end_tag('form');
+
+    // JavaScript for automatic form submission
+    echo html_writer::script("
+        document.addEventListener('DOMContentLoaded', function() {
+            var statusSelect = document.getElementById('status-select');
+            var categorySelect = document.getElementById('category-select');
+            var dateInput = document.querySelector('.date-input');
+            var form = document.getElementById('filter-form');
+
+            function submitForm() {
+                form.submit();
+            }
+
+            if (statusSelect) {
+                statusSelect.addEventListener('change', submitForm);
+            }
+
+            if (categorySelect) {
+                categorySelect.addEventListener('change', submitForm);
+            }
+
+            if (dateInput) {
+                dateInput.addEventListener('change', submitForm);
+            }
+        });
+    ");
+?>
