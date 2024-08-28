bf83ab5cfbcc4d2cd451a2aeaad663bfbdd3c875
Gaurav Gusain <gaurav.gusain@coloredcow.in>
2024-08-28 16:17:14 +0530

    assign school to course functionality

--- moodle_courses/templates/manage_course_table.php
+++ moodle_courses/templates/manage_course_table.php
@@ -6,24 +6,32 @@ $helper = new \theme_academi\helper();
 $table->head = [
     'Name',
     'Category',
-    "LEVEL",
     'Created ON',
     'Schools',
     'Teachers assigned',
-    'Status'
+    'Status',
+    'Action'
 ];
 
 foreach ($courses as $course) {
     $editurl = new moodle_url('/course/edit.php', ['id' => $course->id]);
+    $assignurl = new moodle_url('/theme/academi/moodle_courses/assign_school.php', ['course' => $course->id]);
     $coursename = html_writer::link($editurl, $course->fullname);
+    $assignbutton = html_writer::div(
+        html_writer::link(
+            $assignurl,
+            'Assign Schools',
+            array('class' => 'mr-2 assign-school-button')
+        )
+    );
     $table->data[] = [
         $coursename,
         format_string($helper->get_category_of_course($course)->name),
-        format_string('-'),
         format_string(date('Y-m-d', $course->timecreated)),
         format_string($helper->get_schools_count_for_course($course)),
         format_string($helper->get_assignees_count_for_course($course)),
-        html_writer::span('Live', "badge badge-pill badge-color survey-status survey-live")
+        html_writer::span('Live', "badge badge-pill badge-color survey-status survey-live"),
+        $assignbutton
     ];
 }
 
