e92db0af0ad386b91f9479888dc3688a3067ef6d
Gaurav Gusain <gaurav.gusain@coloredcow.in>
2024-08-30 19:27:27 +0530

    css fix

--- moodle_courses/templates/manage_course_table.php
+++ moodle_courses/templates/manage_course_table.php
@@ -22,9 +22,10 @@ foreach ($courses as $course) {
     $editsettingurl = new moodle_url('/course/edit.php', ['id' => $course->id]);
     $editcourseurl = new moodle_url('/course/view.php', ['id' => $course->id]);
     $assignurl = new moodle_url('/theme/academi/moodle_courses/assign_school.php', ['course' => $course->id]);
-    $coursename = html_writer::link($editsettingurl, $course->fullname);
+    $coursename = $course->fullname;
     $assignbutton = '';
     if (has_capability('local/moodle_survey:assign-course-to-school', context_system::instance())) {
+        $coursename = html_writer::link($editsettingurl, $course->fullname);
         $assignbutton = html_writer::div(
             html_writer::link(
                 $editcourseurl,
@@ -44,6 +45,11 @@ foreach ($courses as $course) {
             html_writer::link(
                 $assignurl,
                 'Assign to Students',
+                array('class' => 'mr-2 assign-school-button')
+            ).
+            html_writer::link(
+                new moodle_url('/course/view.php', ['id' => $course->id]),
+                'View',
                 array('class' => 'mr-2 assign-school-button')
             )
         );
